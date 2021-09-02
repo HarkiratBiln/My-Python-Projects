c44a5be445db29ca94454eb18ac67ecf4e3352dd
HarkiratBiln <harkiratbiln@gmail.com>
2021-09-01 17:58:20 -0700

    Finished swapping files project

---
+++ swappingFile.py
@@ -0,0 +1,23 @@
+def swapFileData():
+    file1 = input("Enter the file name: ")
+    file2 = input("Enter the file name: ")
+
+    with open(file1, "r") as a:
+        data_a = a.read()
+    with open(file2, "r") as a:
+        data_b = a.read() 
+
+
+    with open(file2, 'w') as a:
+        a.write(data_a)
+    with open(file1, "w") as a:
+        a.write(data_b)
+
+
+
+swapFileData()
+
+
+
+
+
